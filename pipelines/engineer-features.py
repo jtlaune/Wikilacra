1116944e0494159a6048630d274902760ea757e7
JT <jtlaune@gmail.com>
2025-10-27 21:09:47 +0000

    WIP: Put new feature functions in, clean up, document

--- pipelines/engineer-features.py
+++ pipelines/engineer-features.py
@@ -5,63 +5,71 @@ Engineer features.
 import sys
 from extract_data_to_label import load_and_clean
 import pandas as pd
+from wikilacra import MEDIAWIKI_HISTOR_DUMP_COL_NAMES
 from wikilacra.features import (
-    create_feature_columns,
-    engineer_features_by_event,
-    entropy_from_series,
+    get_glob_quants,
+    get_page_quants,
+    construct_counts,
+    compute_windows,
+    get_lagged_and_slopes,
+    compute_intracounts_stats,
 )
+from wikilacra.stream import read_data_chunked
 
 if __name__ == "__main__":
+    # Inputs
     rolling_avg_hrs = int(sys.argv[1])
     dump_input_file = sys.argv[2]
     labels_only_input_file = sys.argv[3]
     counts_path = sys.argv[4]
     output_path = sys.argv[5]
 
+    # Get the labeled data
     labels = pd.read_csv(labels_only_input_file, index_col=0)
+    # Selected titles for reading all columns
+    sel_titles = set(labels["page_title"].unique())
 
-    columns_to_keep = [
-        "event_timestamp",
-        "page_title",
-        "event_user_id",
-        "event_user_text_historical",
-        "page_id",
-        "page_is_deleted",
-        "event_comment",
-    ]
-    columns_to_read = [
-        "event_entity",
-        "page_namespace",
-        *columns_to_keep,
-    ]
-    revisions = load_and_clean(
+    # Read in only 2 columns for the whole revisisions list to save memory
+    revisions_glob = read_data_chunked(
         dump_input_file,
-        columns_to_keep,
-        columns_to_read,
-    )
-    counts = pd.read_csv(counts_path)
-    counts["event_timestamp"] = pd.to_datetime(
-        counts["event_timestamp"]
+        ["event_timestamp","page_title"],
+        MEDIAWIKI_HISTOR_DUMP_COL_NAMES,
     )
 
-    colnames = create_feature_columns(labels, rolling_avg_hrs)
-
-    page_entropies = counts.groupby("event_timestamp").agg(
-        test=("revision_count", entropy_from_series)
+    # Read all the columns in for those in the labeled set
+    revisions_sel = read_data_chunked(
+        "../inputs/label_data/2025-08.enwiki.2025-08.tsv",
+        MEDIAWIKI_HISTOR_DUMP_COL_NAMES,
+        MEDIAWIKI_HISTOR_DUMP_COL_NAMES,
+        page_titles=sel_titles,
     )
 
-    labels["event_timestamp"] = pd.to_datetime(labels["event_timestamp"])
+    # Global count quantities
+    dt_glob = get_glob_quants(revisions_glob, "1h")
 
-    for idx, row in labels.iterrows():
-        engineer_features_by_event(
-            row,
-            idx,
-            counts,
-            page_entropies,
-            revisions,
-            labels,
-            rolling_avg_hrs,
-            **colnames,
-        )
+    # Labeled-page-specific count quantities
+    dt_page = get_page_quants(revisions_sel,"1h")
+    counts = construct_counts(
+        dt_page,
+        "2025-08-01 00:00:00", # TODO: make these inputtable 
+        "2025-08-31 23:59:59",
+        "1h",
+    )
 
-    labels.to_csv(output_path)
+    # Numerical columns selection (for rolling, lagged, expwin, and intracounts)
+    num_cols = list(set(counts.columns)-{"page_creation","hrs_since_page_creation"})
+
+    # Rolling & exponential windows
+    roll, expwin = compute_windows(counts, ROLLING_HRS, num_cols)
+
+    # Lags and slopes
+    lags, slopes = get_lagged_and_slopes(counts, ROLLING_HRS, num_cols)
+
+    # Intra-hour statistics, e.g. max burst score
+    intracounts = compute_intracounts_stats(
+        revisions_sel, INTRA_PD, INTRA_WIN
+    )
+
+    # TODO: write all of it into `engineered` dataframe.
+
+    engineered.to_csv(output_path)
